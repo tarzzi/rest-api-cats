e1157cdb0946e99cbe11cf5e7dcc42188ba7c413
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2021-08-25 12:50:32 +0300

    Update links, styles

--- frontend/next-rest-cats/pages/index.js
+++ frontend/next-rest-cats/pages/index.js
@@ -1,29 +1,52 @@
-import Head from 'next/head'
+import Link from "next/link";
+import Head from "next/head";
 
-export default function Home() {
+export default function IndexPage() {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Rest cats</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Resting cats</title>
       </Head>
-
-      <main>
-        
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f3f4f5"
+          fillOpacity="1"
+          d="M0,32L40,58.7C80,85,160,139,240,149.3C320,160,400,128,480,133.3C560,139,640,181,720,181.3C800,181,880,139,960,138.7C1040,139,1120,181,1200,192C1280,203,1360,181,1400,170.7L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+        ></path>
+      </svg>
+      <div className="intro">
+        <h1>Rest api cats</h1>
+        <p>Demo for rest api</p>
+      </div>
+      <div className="grid">
+        <Link href="/cats">All cats</Link>
+        <Link href="/cats/edit">Edit a cat</Link>
+      </div>
+      <div className="footer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#f3f4f5"
+            fillOpacity="1"
+            d="M0,128L40,154.7C80,181,160,235,240,218.7C320,203,400,117,480,96C560,75,640,117,720,144C800,171,880,181,960,160C1040,139,1120,85,1200,58.7C1280,32,1360,32,1400,32L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
       <style jsx>{`
+        .intro h1,
+        p {
+          margin: 0;
+          padding: 0;
+          text-align: center;
+        }
+        .grid {
+          margin: 2rem 0;
+          display: grid;
+          grid-template-columns: auto auto;
+          justify-content: center;
+          align-items: center;
+          gap: 5rem;
+          font-size: 1.5rem;
+        }
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -32,7 +55,6 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -41,58 +63,49 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
         footer {
           width: 100%;
-          height: 100px;
           border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
+          position: fixed;
+          bottom: 0px;
         }
-
         footer img {
           margin-left: 0.5rem;
         }
-
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
-
         a {
           color: inherit;
           text-decoration: none;
         }
-
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
-
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
-
         .title,
         .description {
           text-align: center;
         }
-
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -101,17 +114,6 @@ export default function Home() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -123,29 +125,24 @@ export default function Home() {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
-
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
-
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
-
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
-
         .logo {
           height: 1em;
         }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -153,7 +150,6 @@ export default function Home() {
           }
         }
       `}</style>
-
       <style jsx global>{`
         html,
         body {
@@ -163,11 +159,21 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
+        a {
+          list-style-type: none;
+          text-decoration: none;
+          color: black;
+        }
+        a:visited {
+          color: black;
+        }
+        a:hover {
+          text-decoration: underline;
+        }
       `}</style>
     </div>
-  )
+  );
 }
