e5d7998ef5a29bbf9b9b10de5be6e7730184b75d
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2021-08-25 13:36:13 +0300

    Create Card.js

---
+++ frontend/next-rest-cats/Card.js
@@ -0,0 +1,60 @@
+export default function Card(cat) {
+  return (
+    <div className="card">
+      <h2>{cat.username}</h2>
+      <p>{cat.name}</p>
+      <p>{cat.address.city}</p>
+      <style jsx>{`
+        * {
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          box-sizing: border-box;
+        }
+        p {
+          margin: 0;
+        }
+        h2 {
+          margin: 0;
+        }
+        a {
+          list-style-type: none;
+          text-decoration: none;
+          color: black;
+        }
+        a:visited {
+          color: black;
+        }
+        a:hover {
+          text-decoration: underline;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}</style>
+    </div>
+  );
+}
